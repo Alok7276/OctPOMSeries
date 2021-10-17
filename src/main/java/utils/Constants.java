1ebd23b28f45727f4cba6ff27ff7e2b30e41e84b
gyanesh <kamal.gyanesh8@gmail.com>
2021-10-17 20:05:02 +0530

    17 Oct Code

--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -2,7 +2,7 @@ package utils;
 
 public class Constants {
 
-	public static final String config = "E:\\Advanced Automation\\SeptPOM\\src\\test\\resources\\config.properties";
+	public static final String config = System.getProperty("user.dir")+"/src/test/resources/config.properties/";
 	public static final int DEFAULT_TIME_OUT = 5;
 	public static final String LOGIN_PAGE_TITLE = "OrangeHRM";
 	public static final String path = System.getProperty("user.dir") + "/screenshot/" + System.currentTimeMillis() + ".png";
